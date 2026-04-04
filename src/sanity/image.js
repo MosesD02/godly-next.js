9f943d5ab528ef06accea479ef0e23d7c6ce59eb
Saad Iqbal <saadiqbal.dev@outlook.com>
2026-04-04 20:19:39 +0500

    fix package issues

--- src/sanity/image.js
+++ src/sanity/image.js
@@ -1,4 +1,4 @@
-import createImageUrlBuilder from "@sanity/image-url";
+import { createImageUrlBuilder } from "@sanity/image-url";
 
 /**
  * Standalone image URL builder — uses only project config so it can run in
