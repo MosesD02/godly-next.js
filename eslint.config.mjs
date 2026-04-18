9a4a145f9f44f91f4788732e0c5a58b70c331138
Saad Iqbal <saadiqbal.dev@outlook.com>
2026-04-18 11:31:34 +0500

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,16 @@
-import nextConfig from "eslint-config-next";
-
-export default [...nextConfig];
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+ 
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+])
+ 
+export default eslintConfig
