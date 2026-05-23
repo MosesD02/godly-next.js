b3e1bede53a5028531fed9f570a6e2996f4fd876
Saad Iqbal <saadiqbal.dev@outlook.com>
2026-05-23 14:48:12 +0500

    add gtm

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono, Inter } from "next/font/google";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import "./globals.css";
 import { AppWrapper } from "@/context/godlyContext";
 import ThirdPartyScripts from "@/components/ThirdPartyScripts";
@@ -58,6 +58,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-5R9DBDVW" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${inter.variable}`}
         suppressHydrationWarning
